16eba827c078ee414773b59c055ce53959c8e95a
Kuby <kuby@cocho.io>
2022-10-15 23:42:14 +0900

    gh-5-sample

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { useQuery } from "@tanstack/react-query";
+import httpClient from "../libs/http-client";
 
 function createData(
   id: number,
@@ -28,8 +30,63 @@ const rows = [
   createData(5, "글제목5", "닉네임5", "2022-01-05"),
 ];
 
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at?: string;
+  author_id: string;
+}
+
+interface Meta {
+  totalItems: number;
+  itemCount: number;
+  itemsPerPage: number;
+  totalPages: number;
+  currentPage: number;
+}
+
+const json = {
+  items: [
+    {
+      id: 1,
+      title: "string",
+      content: "string",
+      created_at: "2022-10-14T16:10:28.993Z",
+      updated_at: "2022-10-14T16:10:28.993Z",
+      deleted_at: null,
+      author_id: "string",
+    },
+  ],
+  meta: {
+    totalItems: 1,
+    itemCount: 1,
+    itemsPerPage: 10,
+    totalPages: 1,
+    currentPage: 1,
+  },
+};
+
+interface PostResponse {
+  items: Post[];
+  meta: Meta;
+}
+
+function usePosts() {
+  return useQuery(["usePosts"], async () => {
+    const { data } = await httpClient.get<PostResponse>("/posts");
+    return data;
+  });
+}
+
 export default function IndexPage() {
   const navigate = useNavigate();
+
+  const { data } = usePosts();
+
+  console.log(data);
 
   return (
     <Layout>
@@ -46,7 +103,7 @@ export default function IndexPage() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {data?.items.map((row) => (
               <TableRow
                 key={row.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
