4d497321d9acbeb9b00620735a2b548a18bb013b
Lizzie <leejihye7117@gmail.com>
2022-10-17 02:27:48 +0900

    gh-6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,25 +14,6 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import httpClient from "../libs/http-client";
 
-/*
-function createData(
-  id: number,
-  title: string,
-  author_id: string,
-  created_at: string
-) {
-  return { id, title, author_id, created_at };
-}
-
-const rows = [
-  createData(1, "글제목1", "닉네임1", "2022-01-01"),
-  createData(2, "글제목2", "닉네임2", "2022-01-02"),
-  createData(3, "글제목3", "닉네임3", "2022-01-03"),
-  createData(4, "글제목4", "닉네임4", "2022-01-04"),
-  createData(5, "글제목5", "닉네임5", "2022-01-05"),
-];
-*/
-
 // 데이터 불러오기
 function usePosts() {
   return useQuery(["usePosts"], async () => {
@@ -61,46 +42,17 @@ interface Meta {
   totalPages: number;
 }
 
-/*
-const json = {
-  items: [
-    {
-      author_id: "string",
-      content: "string",
-      created_at: "2022-10-14T16:10:28.993Z",
-      deleted_at: null,
-      id: 1,
-      title: "string",
-      updated_at: "2022-10-14T16:10:28.993Z",
-    },
-  ],
-  meta: {
-    currentPage: 1,
-    itemCount: 1,
-    itemsPerPage: 10,
-    totalItems: 1,
-    totalPages: 1,
-  },
-};
-*/
-
 interface PostResponse {
   items: Post[];
   meta: Meta;
 }
 
 export default function IndexPage() {
-  const navigate = useNavigate();
-
-  usePosts();
-
   const { data } = usePosts();
   console.log("data :", data);
 
   return (
     <Layout>
-      {/* hello index page */}
-      {/* <button onClick={() => navigate("/posts/1")}>go to detail 1</button> */}
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
           <TableHead>
