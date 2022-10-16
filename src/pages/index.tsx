bb49ccac60bb83155f84f572f34584c69ff82c24
Lizzie <100846243+2wisdom@users.noreply.github.com>
2022-10-17 02:33:54 +0900

    gh-5 글목록 데이터 & 링크 연결 (#6)
    * gh-5 글목록 데이터 연동
    
    * gh-5 글목록 링크 연결
    
    * gh-6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import Layout from "../layout/Layout";
+import { Link } from "react-router-dom";
 
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -10,31 +12,47 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import httpClient from "../libs/http-client";
 
-function createData(
-  id: number,
-  title: string,
-  author_id: string,
-  created_at: string
-) {
-  return { id, title, author_id, created_at };
+// 데이터 불러오기
+function usePosts() {
+  return useQuery(["usePosts"], async () => {
+    const { data } = await httpClient.get<PostResponse>("/posts");
+    return data;
+  });
 }
 
-const rows = [
-  createData(1, "글제목1", "닉네임1", "2022-01-01"),
-  createData(2, "글제목2", "닉네임2", "2022-01-02"),
-  createData(3, "글제목3", "닉네임3", "2022-01-03"),
-  createData(4, "글제목4", "닉네임4", "2022-01-04"),
-  createData(5, "글제목5", "닉네임5", "2022-01-05"),
-];
+// 게시글
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  author_id: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at?: string;
+}
+
+// 페이지네이션
+interface Meta {
+  currentPage: number;
+  itemCount: number;
+  itemsPerPage: number;
+  totalItems: number;
+  totalPages: number;
+}
+
+interface PostResponse {
+  items: Post[];
+  meta: Meta;
+}
 
 export default function IndexPage() {
-  const navigate = useNavigate();
+  const { data } = usePosts();
+  console.log("data :", data);
 
   return (
     <Layout>
-      {/* hello index page
-      <button onClick={() => navigate("/posts/1")}>go to detail 1</button> */}
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
           <TableHead>
@@ -46,7 +64,7 @@ export default function IndexPage() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {data?.items.map((row) => (
               <TableRow
                 key={row.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -54,7 +72,9 @@ export default function IndexPage() {
                 <TableCell component="th" scope="row">
                   {row.id}
                 </TableCell>
-                <TableCell align="right">{row.title}</TableCell>
+                <Link to={`/posts/${row.id}`}>
+                  <TableCell align="right">{row.title}</TableCell>
+                </Link>
                 <TableCell align="right">{row.author_id}</TableCell>
                 <TableCell align="right">{row.created_at}</TableCell>
               </TableRow>
