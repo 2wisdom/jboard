9a44428986f444a09d47a0820e23ac82fccd5073
Lizzie <leejihye7117@gmail.com>
2022-10-21 01:04:19 +0900

    gh-7 회원가입 UI

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,90 @@
+import * as React from "react";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { Controller } from "react-hook-form";
+import { useFormAction } from "react-router-dom";
+import Layout from "../layout/Layout";
+import { useQuery } from "@tanstack/react-query";
+import httpClient from "../libs/http-client";
+import { Box } from "@mui/system";
+
 export default function SignUpPage() {
+  // 데이터 불러오기
+  function useSignUp() {
+    return useQuery(["useSignUp"], async () => {
+      const { data } = await httpClient.post<user>("/user");
+      return data;
+    });
+  }
+
+  // user interface
+  interface user {
+    email: string;
+    password: string;
+    nickname: string;
+  }
+
   return (
-    <div>
-      <h1>signUp</h1>
-    </div>
+    <Layout>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Container
+          sx={{
+            p: 3,
+            m: 10,
+          }}
+        >
+          <Box component="div">
+            <Typography>SignUp</Typography>
+          </Box>
+          <Box component="form">
+            <TextField
+              required
+              type="name"
+              id="name"
+              label="name"
+              variant="standard"
+              // sx={{ m: 3 }}
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              required
+              type="email"
+              id="email"
+              label="email"
+              variant="standard"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              required
+              type="password"
+              id="password"
+              label="password"
+              variant="standard"
+              margin="normal"
+              fullWidth
+            />
+            <TextField
+              required
+              type="password"
+              id="ConfirmPassword"
+              label="ConfirmPassword"
+              variant="standard"
+              margin="normal"
+              fullWidth
+            />
+          </Box>
+          <Button variant="outlined" sx={{ m: 5 }}>
+            SignUp
+          </Button>
+        </Container>
+      </Grid>
+    </Layout>
   );
 }
