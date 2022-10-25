768c13f386255b45fa5c6c54a35d9ab328e4c168
Lizzie <100846243+2wisdom@users.noreply.github.com>
2022-10-25 19:11:43 +0900

    gh-8 회원가입 UI (#9)
    * gh-7 회원가입 UI
    
    * gh-7 회원가입 UI
    
    * gh-8 수정
    
    * gh-8 interface 함수 수정

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,7 +1,92 @@
+import * as React from "react";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { Controller } from "react-hook-form";
+import { useFormAction } from "react-router-dom";
+import Layout from "../layout/Layout";
+import { useQuery } from "@tanstack/react-query";
+import httpClient from "../libs/http-client";
+import { Box } from "@mui/system";
+
+// user interface
+interface User {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
 export default function SignUpPage() {
   return (
-    <div>
-      <h1>signUp</h1>
-    </div>
+    <Layout>
+      <Container component="main" maxWidth="xs">
+        <form>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h6">
+              Sign Up
+            </Typography>
+
+            <Box
+              component="form"
+              noValidate
+              // onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <TextField
+                name="name"
+                label="name"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                autoFocus
+              />
+
+              <TextField
+                name="email"
+                label="email"
+                variant="outlined"
+                autoComplete="email"
+                margin="normal"
+                fullWidth
+              />
+
+              <TextField
+                name="password"
+                label="password"
+                variant="outlined"
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                fullWidth
+              />
+
+              <TextField
+                name="confirmPassword"
+                label="confirm password"
+                variant="outlined"
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                fullWidth
+              />
+
+              <Button
+                variant="contained"
+                type="submit"
+                fullWidth
+                sx={{ mt: 3, mb: 2 }}
+              >
+                SignUp !
+              </Button>
+            </Box>
+          </Box>
+        </form>
+      </Container>
+    </Layout>
   );
 }
